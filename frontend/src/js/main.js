0e7b101203eeb1253e0a14ec4bc7301192698924
Chaitya Shah <Chaitya62@gmail.com>
2017-10-29 02:10:30 +0530

    Add get_data function

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom';
 import AppAPI from './utils/appAPI';
 import {HashRouter, Route, Redirect} from 'react-router-dom';
 
+AppAPI.get_data("http://localhost/forum", null, 'GET', function(data, err){
+	if(err) console.log("There was an error ");
+	console.log(data);
+	console.log("Hello, World!");
+});
 
 ReactDOM.render(
   <HashRouter>
--- frontend/src/js/utils/appAPI.js
+++ frontend/src/js/utils/appAPI.js
@@ -3,4 +3,16 @@ var AppActions = require('../actions/AppActions');
 
 module.exports = {
 	
+	 "get_data" : function(url, params, protocol,callback){
+		var httpx = new XMLHttpRequest();
+		httpx.onreadystatechange = function(){
+			if(this.readyState == 4 && this.status == 200){
+				callback(this.response, null);
+			}
+		}
+		httpx.open(protocol, url, false);
+		httpx.send(params);
+	}
+
+
 }
